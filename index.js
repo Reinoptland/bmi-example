200775d77bd3e1f67abf7fb081cf6bb20771fa44
Reinoptland <reinoptland@gmail.com>
2020-04-21 17:31:53 +0200

    Generate output for user, remove hardcoded arguments

--- index.js
+++ index.js
@@ -2,15 +2,30 @@ function bmiCalculator() {
   const height = parseFloat(process.argv[2]);
   const weight = parseInt(process.argv[3]);
 
-  calculateBMI(1.6, 70); // pass arguments
+  var BMI = calculateBMI(height, weight); // pass arguments
+  console.log(BMI);
   //   console.log("HEIGHT:", height, "WEIGHT", weight);
+
+  console.log(`
+  ************
+  HELLO THERE
+  ************
+
+  STATS
+  *****
+  Weight = ${weight} kg
+  Height = ${height} m
+  Your BMI = ${Math.round(BMI)}
+  
+  `);
 }
 
 function calculateBMI(heightInM, weightInKg) {
   console.log("HEIGHT:", heightInM, "WEIGHT:", weightInKg);
   // weight won't work, it is not in scope
   var BMI = weightInKg / (heightInM * heightInM);
-  console.log(BMI);
+
+  return BMI;
 }
 
 bmiCalculator();
--- plan.md
+++ plan.md
@@ -5,8 +5,9 @@
 X input: height, weight, process.argv
 X store the input
 
-- processing
-  - calculate the BMI using a function
-  - perform a calculation inside of function
-  - return the value from the function
+X processing
+X calculate the BMI using a function
+X perform a calculation inside of function
+X return the value from the function
+
 - use the return value, give some output
