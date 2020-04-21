5804fd8677cfae4d1e84b963f86e65a8d06f742a
Reinoptland <reinoptland@gmail.com>
2020-04-21 17:21:26 +0200

    Pass arguments into function

--- index.js
+++ index.js
@@ -2,7 +2,13 @@ function bmiCalculator() {
   const height = parseFloat(process.argv[2]);
   const weight = parseInt(process.argv[3]);
 
-  console.log("HEIGHT:", height, "WEIGHT", weight);
+  calculateBMI(1.6, 70); // pass arguments
+  //   console.log("HEIGHT:", height, "WEIGHT", weight);
+}
+
+function calculateBMI(heightInM, weightInKg) {
+  // define parameters
+  console.log("HEIGHT:", heightInM, "WEIGHT:", weightInKg);
 }
 
 bmiCalculator();
